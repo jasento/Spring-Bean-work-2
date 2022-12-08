f32fdece54f2373e1c8d537306106c632a118403
李国升 <guosheng.li@pactera.com>
2022-12-08 17:45:47 +0900

    update

---
+++ src/main/java/com/example/demo/req/test02Req.java
@@ -0,0 +1,14 @@
+package com.example.demo.req;
+
+import lombok.Data;
+
+@Data
+public class test02Req {
+
+
+    private String id;
+
+
+    private String name;
+
+}
---
+++ src/main/java/com/example/demo/work2/CustomProperties.java
@@ -0,0 +1,32 @@
+package com.example.demo.work2;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "app")
+@Data
+public class CustomProperties {
+
+    public static String appid;
+    public static String appkey;
+    public static String secretkey;
+
+    @Value("${app.appid}")
+    public void setAppid(String appid) {
+        CustomProperties.appid = appid;
+    }
+    @Value("${app.appkey}")
+    public void setAppkey(String appkey) {
+        CustomProperties.appkey = appkey;
+    }
+    @Value("${app.secretkey}")
+    public void setSecretkey(String secretkey) {
+        CustomProperties.secretkey = secretkey;
+    }
+
+}
+
+
+
